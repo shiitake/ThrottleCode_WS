1fed4a06efa138d5f2542f1d4d25467dbfde4cab
Shannon Barrett <shannon.barrett@dealertrack.com>
2014-01-06 12:04:52 -0600

    Updated project with working version

---
+++ ThrottleCode_WS/ThrottleCode_WS/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ThrottleCode_WS.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <style>
+    .Over { overflow: auto; width: 95%; }
+</style>
+
+    <form id="form1" runat="server">
+        <p>
+            <asp:Table ID="Table1" runat="server" BorderStyle="None" Width="483px">
+                <asp:TableRow runat="server" BorderStyle="Solid" HorizontalAlign="Left">
+                    <asp:TableCell runat="server">Insert the Throttling Reason Code:</asp:TableCell>
+                    <asp:TableCell runat="server"><asp:TextBox ID="reason" ReadOnly="False" runat="server" Width="200px"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server" BorderColor="Black" BorderStyle="Solid">
+                    <asp:TableCell runat="server">Throttling Mode:</asp:TableCell>
+                    <asp:TableCell runat="server"><asp:TextBox ID="throttleMode" ReadOnly="True" runat="server" Width="200px"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server" BorderStyle="Solid">
+                    <asp:TableCell runat="server">Throttling Type:</asp:TableCell>
+                    <asp:TableCell runat="server"><asp:TextBox ID="throttleType" ReadOnly="True" runat="server" Width="200px"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow runat="server" BorderStyle="Solid">
+                    <asp:TableCell runat="server">Throttling Resource</asp:TableCell>
+                    <asp:TableCell runat="server"><asp:TextBox ID="resourceType" ReadOnly="True" runat="server" Width="200px"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+            </asp:Table></p>
+            <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" /> 
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="clear" />
+        </p>
+        <asp:TextBox ID="errorBox" runat="server" BorderStyle="None" BorderWidth="0px" Height="241px" 
+            ReadOnly="True" TextMode="MultiLine" Rows="10" Width="441px" CssClass="Over" ForeColor ="red"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ ThrottleCode_WS/ThrottleCode_WS/Default.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ThrottleCode_WS
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clearFields();
+                throttleMode.Text = calcMode(Convert.ToInt32(reason.Text));
+                int myLoops;
+                string resType;
+                string throtType;
+                calcResource(Convert.ToInt32(reason.Text), out myLoops, out resType);
+                typeFunction(myLoops, Convert.ToInt32(reason.Text), out throtType);                
+                //resourceType.Text = typeFunction(Convert.ToInt32(reason.Text));
+                throttleType.Text = throtType;
+                resourceType.Text = resType;
+            }
+            catch (ArithmeticException z)
+            {
+                throttleMode.Text = "see below";
+                throttleType.Text = "see below";
+                resourceType.Text = "see below";
+                errorBox.Text = "Error: " + z.Message + "\r\nPlease verify that you are using a valid reason code and try your request again.";
+            }      
+                
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            clearFields();
+            reason.Text = "";
+        }
+
+        static public string calcMode(int mode)
+        {
+            switch (mode % 4)
+            {
+                case 0: return "No throttling";
+                case 1: return "Reject Updates and Inserts";
+                case 2: return "Reject All Writes";
+                case 3: return "Reject All Reads and Writes";
+                default: return "Unknown.";
+            }
+        }
+        
+        static public void calcResource(int reasonCode, out int myLoops, out string resType)
+        {
+        myLoops = 0;
+        int holderCode = (reasonCode / 256);
+            //determine loops value            
+                do{
+                    if(holderCode == 0)
+                    {
+                        myLoops = -1;
+                        resType = "(see below)";
+                        throw new ArithmeticException();                        
+                    }
+                    else if (holderCode % 2 > 0)
+                    {
+                        holderCode = -1;
+                    }
+                    else{
+                        myLoops++;
+                        holderCode /= 2;
+                    }
+                }
+                while (holderCode > -1);
+            
+           //resource type calculation
+            switch(Convert.ToInt32(myLoops/2))
+            {
+                case 0: resType = "Physical Database Space"; break;
+                case 1: resType = "Physical Log Space"; break;
+                case 2: resType = "LogWriteIODelay"; break;
+                case 3: resType = "DataReadIODelay"; break;
+                case 4: resType = "CPU"; break;
+                case 5: resType = "Database Size"; break;
+                case 6: resType = "Internal"; break;
+                case 7: resType = "SQL Worker Threads"; break;
+                case 8: resType = "Internal"; break;
+                default: resType = "Resource Type is unknown."; break;
+            }
+            myLoops++;
+            return;
+            }
+           
+        static public void typeFunction(int myLoops, int mode, out string throtType)
+        {
+            if (myLoops == -1)
+            {
+                throtType = "(see below)";
+            }
+            else if (myLoops % 2 == 0 && mode > 0)
+            {
+                throtType = "Hard Throttle";
+            }
+            else if (mode > 0)
+            {
+                throtType = "Soft Throttle";
+            }
+            else
+            {
+                throtType = "No Throttling";
+            }
+            return;
+            }
+
+        public void clearFields()
+        {
+            throttleMode.Text = "";
+            throttleType.Text = "";
+            resourceType.Text = "";
+            errorBox.Text = "";
+            return;
+        }
+
+    }
+}
